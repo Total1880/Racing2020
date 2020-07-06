94d89c77bf007cd5d47b4d262b797c4994d6b8ee
olav hendrickx <olav.hendrickx@hotmail.com>
2020-07-06 16:14:29 +0200

    Add project files.

---
+++ AddNamesToDatabase/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ExcelDataReader;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace AddNamesToDatabase
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        string path = "C:\\Users\\olavh\\Downloads\\TestNames.xls";
+        DataSet dataSet;
+        private RelayCommand _testCommand;
+        public RelayCommand TestCommand => _testCommand ??= new RelayCommand(Test);
+        List<string> FirstNames = new List<string>();
+        List<string> LastNames = new List<string>();
+
+        private void Test()
+        {
+            if (File.Exists(path))
+            {
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                using var stream = File.Open(path, FileMode.Open, FileAccess.Read);
+                using var reader = ExcelReaderFactory.CreateReader(stream);
+                dataSet = reader.AsDataSet();
+            }
+            var table = dataSet.Tables["names"];
+            var rows = table.Rows;
+
+            foreach (DataRow row in rows)
+            {
+                FirstNames.Add(row[0].ToString());
+                LastNames.Add(row[1].ToString());
+            }
+        }
+    }
+}
---
+++ AddNamesToDatabase/ViewModelLocator.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+
+namespace AddNamesToDatabase
+{
+    public class ViewModelLocator
+    {
+        private readonly IContainer _container;
+
+        public ViewModelLocator()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainWindowViewModel>().SingleInstance();
+
+            _container = builder.Build();
+        }
+
+        public MainWindowViewModel MainWindow => _container.Resolve<MainWindowViewModel>();
+    }
+}
