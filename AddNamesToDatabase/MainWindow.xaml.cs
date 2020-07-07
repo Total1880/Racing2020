6e4ca1e327e64163254f14b7d53795a729278ed9
olav hendrickx <olav.hendrickx@hotmail.com>
2020-07-07 14:49:03 +0200

    Develop WPF

--- AddNamesToDatabase/MainWindow.xaml.cs
+++ AddNamesToDatabase/MainWindow.xaml.cs
@@ -20,9 +20,26 @@ namespace AddNamesToDatabase
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainPage _mainPage;
+        private NationPage _nationPage;
+
+        private MainPage MainPage => _mainPage ??= new MainPage();
+        private NationPage NationPage => _nationPage ??= new NationPage();
+
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.NavigationService.Navigate(MainPage);
+        }
+
+        private void Button_Click_New_List(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(MainPage);
+        }
+
+        private void Button_Click_New_Nation(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(NationPage);
         }
     }
 }
---
+++ AddNamesToDatabase/NationPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AddNamesToDatabase
+{
+    /// <summary>
+    /// Interaction logic for NationPage.xaml
+    /// </summary>
+    public partial class NationPage : Page
+    {
+        public NationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
