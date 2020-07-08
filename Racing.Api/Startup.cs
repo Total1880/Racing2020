c2e7385ecfc27494d507b86cb9c1773d1f67b415
olav hendrickx <olav.hendrickx@hotmail.com>
2020-07-08 14:15:42 +0200

    add swagger

--- Racing.Api/Startup.cs
+++ Racing.Api/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Racing.Api.Repositories;
 using Racing.Api.Services;
 using Racing.Api.Services.Interfaces;
@@ -32,6 +26,11 @@ namespace Racing.Api
             services.AddScoped<INationService, NationService>();
             services.AddScoped<IRepository<Nation>, NationRepository>();
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Racing2020 API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +46,12 @@ namespace Racing.Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Racing2020 API v1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
