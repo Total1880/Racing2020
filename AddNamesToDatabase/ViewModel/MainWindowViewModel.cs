c299b31191c232dede8ea458bb3b9176c4cc8cc1
olav hendrickx <olav.hendrickx@hotmail.com>
2020-07-09 11:45:27 +0200

    update MainWindowViewModel

--- AddNamesToDatabase/ViewModel/MainWindowViewModel.cs
+++ AddNamesToDatabase/ViewModel/MainWindowViewModel.cs
@@ -1,43 +1,25 @@
 ﻿using AddNamesToDatabase.Messages;
 using AddNamesToDatabase.Services.Interfaces;
+using ExcelDataReader;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using Racing.Model;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AddNamesToDatabase.ViewModel
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        //string path = "C:\\Users\\olavh\\Downloads\\TestNames.xls";
-        //DataSet dataSet;
-        //private RelayCommand _testCommand;
-        //public RelayCommand TestCommand => _testCommand ??= new RelayCommand(Test);
-        //List<string> FirstNames = new List<string>();
-        //List<string> LastNames = new List<string>();
-
-        //private void Test()
-        //{
-        //    if (File.Exists(path))
-        //    {
-        //        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-        //        using var stream = File.Open(path, FileMode.Open, FileAccess.Read);
-        //        using var reader = ExcelReaderFactory.CreateReader(stream);
-        //        dataSet = reader.AsDataSet();
-        //    }
-        //    var table = dataSet.Tables["names"];
-        //    var rows = table.Rows;
-
-        //    foreach (DataRow row in rows)
-        //    {
-        //        FirstNames.Add(row[0].ToString());
-        //        LastNames.Add(row[1].ToString());
-        //    }
-        //}
-
         private readonly INationService _nationService;
         private ObservableCollection<Nation> _nationList;
+        private RelayCommand _uploadListCommand;
+        private List<FirstNames> _firstNames = new List<FirstNames>();
+        private List<LastNames> _lastNames = new List<LastNames>();
 
         public ObservableCollection<Nation> NationList
         {
@@ -48,6 +30,12 @@ namespace AddNamesToDatabase.ViewModel
                 RaisePropertyChanged();
             }
         }
+
+        public Nation SelectedNation { get; set; }
+
+        public string Path { get; set; }
+
+        public RelayCommand UploadListCommand => _uploadListCommand ??= new RelayCommand(UploadList);
 
         public MainWindowViewModel(INationService nationService)
         {
@@ -64,6 +52,42 @@ namespace AddNamesToDatabase.ViewModel
         private async Task GetNations()
         {
             NationList = new ObservableCollection<Nation>(await _nationService.GetNations());
+        }
+
+        private void UploadList()
+        {
+            if (File.Exists(Path) && SelectedNation != null)
+            {
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                using var stream = File.Open(Path, FileMode.Open, FileAccess.Read);
+                using var reader = ExcelReaderFactory.CreateReader(stream);
+
+                var dataSetNames = reader.AsDataSet();
+                var table = dataSetNames.Tables["names"];
+                var headerFirstNames = "FirstName";
+                var headerLastNames = "LastName";
+                if (table == null)
+                    return;
+                var rows = table.Rows;
+
+                int index = 0;
+
+                foreach (DataRow row in rows)
+                {
+                    if (index == 0)
+                    {
+                        if (row[0].ToString() != headerFirstNames || row[1].ToString() != headerLastNames)
+                            break;
+                        index++;
+                        continue;
+                    }
+
+                    _firstNames.Add(new FirstNames { FirstName = row[0].ToString(), Nation = SelectedNation });
+                    _lastNames.Add(new LastNames { LastName = row[1].ToString(), Nation = SelectedNation });
+
+                    //TODO: lijsten doorsturen naar db
+                }
+            }
         }
     }
 }
